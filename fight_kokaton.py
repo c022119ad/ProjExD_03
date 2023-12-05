c06f96515c83132b7754505fca2811902c27ec82
c0b22119 <c0b22119ad@edu.teu.ac.jp>
2023-12-05 14:08:01 +0900

    練習1 beam生成

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -89,7 +89,23 @@ class Bird:
         if check_bound(self.rct) != (True, True):
             self.rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(self.img, self.rct)
-
+        
+        
+class Beam:
+    
+    def __init__(self,bird: Bird) :
+        self.img = pg.image.load(f"{MAIN_DIR}/fig/beam.png")
+        self.rct =self.img.get_rect()
+        self.rct.center=bird.rct.center
+        self.vx,self.vy = +5,0
+        
+    def update(self,screen: pg.Surface):
+        """
+        ビームを速度ベクトルself.vs,self.vyに基づき移動させる
+        引数 screen:画面Surface
+        """
+        self.rct.move_ip(self.vx, self.vy)
+        screen.blit(self.img, self.rct)
 
 class Bomb:
     """
@@ -121,20 +137,22 @@ class Bomb:
         self.rct.move_ip(self.vx, self.vy)
         screen.blit(self.img, self.rct)
 
-
+    
 def main():
     pg.display.set_caption("たたかえ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))    
     bg_img = pg.image.load(f"{MAIN_DIR}/fig/pg_bg.jpg")
     bird = Bird(3, (900, 400))
     bomb = Bomb((255, 0, 0), 10)
-
+    beam = 0
     clock = pg.time.Clock()
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
+            if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:  #スペースキーが押されたら
+                beam = Beam(bird)  #ビームインスタンスの生成
         
         screen.blit(bg_img, [0, 0])
         
@@ -147,6 +165,8 @@ def main():
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
+        if beam:
+            beam.update(screen)
         bomb.update(screen)
         pg.display.update()
         tmr += 1
